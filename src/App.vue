6914c3054e9eb377f1ea0dcee4a85d6b733b1744
Balakrishna Prasad Ganne <balkripra.1996@gmail.com>
2021-06-05 15:39:09 +0530

    Will write all the components in Home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-  <VButton v-text="'Hello'" @click="btn" />
-
+  <Home />
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import VButton from './components/Button.vue'
+<script>
+import Home from './pages/Home.vue'
+
+export default{
+  components: {Home}
+}
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
---
+++ src/pages/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+
+  <VButton v-text="'Hello123'" @click="alertThis('Clicked the Button')" />
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld.vue'
+import VButton from '../components/Button.vue'
+export default {
+    components: {
+        HelloWorld,
+        VButton
+    },
+methods: {
+    alertThis(msg) {
+        alert(`Here is the msg - ${msg}`)
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
