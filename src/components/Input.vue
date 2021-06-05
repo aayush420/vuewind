b1d9b5fe26ad1302211035b2db525fb55d7f5255
Balakrishna Prasad Ganne <balkripra.1996@gmail.com>
2021-06-05 17:28:59 +0530

    Added a simple text input

---
+++ src/components/Input.vue
@@ -0,0 +1,13 @@
+<template>
+  <input class="outline-none bg-indigo-100 text-indigo-900 p-2 rounded focus:bg-indigo-200" />
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,6 +3,7 @@
         <HelloWorld msg="Hello Vue 3 + Vite" />
         <VAlert text="Hello from the alert" />
         <VButton v-text="'Hello123'" @click="alertThis('Clicked the Button')" />
+        <VInput placeholder="Enter Something Here" class="mx-2" />
     </div>
 </template>
 
@@ -10,11 +11,13 @@
 import HelloWorld from "../components/HelloWorld.vue";
 import VButton from "../components/Button.vue";
 import VAlert from "../components/Alert.vue";
+import VInput from "../components/Input.vue";
 export default {
     components: {
         HelloWorld,
         VButton,
         VAlert,
+        VInput
     },
     methods: {
         alertThis(msg) {
